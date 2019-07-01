71dc300cb54ce0f7260e3e74365a9278effa175d
shayaanakbar <shayaanakbar1106@gmail.com>
2019-06-30 19:12:14 -0500

    set up index.js file & server.js file

---
+++ index.js
@@ -0,0 +1,6 @@
+const server = require('./games-api/server');
+
+const port = process.env.PORT || 7000;
+
+server.listen(port, () => console.log(`server is listening on port \n --**-- ${port} --**--\n`));
+
